732f004a552a34326c10e750f60091b0d3317fbb
LiubovErm <105315327+LiubovErm@users.noreply.github.com>
2022-10-29 14:05:37 +0300

    useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,51 @@
-import { Component } from 'react';
-
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const options = ['good', 'neutral', 'bad'];
+ 
 
-  onClickFeedBack = feedback => {
-    this.setState(prevState => ({ [feedback]: (prevState[feedback] += 1) }));
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
+  
+  const totalFeedback = countTotalFeedback();
 
-  countTotalFeedback() {
-    return Object.values(this.state).reduce((acc, num) => acc + num, 0);
+  const countPositiveFeedbackPercentage = () => {
+    return Math.ceil((good /countTotalFeedback()) * 100) || 0;
   }
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.ceil((this.state.good / this.countTotalFeedback()) * 100) || 0;
+  const onClickFeedBack = (feedback) => {
+    switch (feedback) {
+      case 'good':
+        setGood(prevState => (prevState += 1));
+        break;
+
+      case 'neutral':
+        setNeutral(prevState => (prevState += 1));
+        break;
+
+      case 'bad':
+        setBad(prevState => (prevState += 1));
+        break;
+
+      default:
+        console.log('error');
+    }
   }
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-    const totalFeedback = this.countTotalFeedback();
-
-    return (
+  return (
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
             options={options}
-            onClickFeedBack={this.onClickFeedBack}
+            onClickFeedBack={onClickFeedBack}
           />
         </Section>
 
@@ -45,13 +56,12 @@ export class App extends Component {
               neutral={neutral}
               bad={bad}
               total={totalFeedback}
-              positiveFeedbackPercentage={this.countPositiveFeedbackPercentage()}
+              positiveFeedbackPercentage={countPositiveFeedbackPercentage()}
             />
            : 
             <Notification message="There is no feedback" />
           }
         </Section>
       </>
-    );
-  }
+  )
 }
